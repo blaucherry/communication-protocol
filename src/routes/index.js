4f293737052b03225d751d37bb2fbc8cb353bd6c
blaucherry <angi.alcantar@hotmail.com>
2024-10-26 03:32:46 -0600

    Actualización de interfaces

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const router = Router()
 router.get('/', (req, res) => res.render('', {title:'Protocolo de comunicacion'}))
 router.get('/signin', (req, res) => res.render('signin', {title: 'Login'}))
 router.get('/signup', (req, res) => res.render('signup',{title: 'Registro'}))
+router.get('/chat',(req,res)=> res.render('chat',{title: 'Chat'}) )
 
 export default router
